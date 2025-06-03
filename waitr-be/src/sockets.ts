ad7a5e7d409642ca19cad8520e99c10f84e8d5f6
Aleksandruu <mischie.andrey@gmail.com>
2025-06-03 10:53:42 +0300

    fixing sockets cors

--- waitr-be/src/sockets.ts
+++ waitr-be/src/sockets.ts
@@ -6,7 +6,7 @@ let io: Server;
 export const initSocket = (server: HttpServer) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "*",
       credentials: true,
     },
     transports: ["websocket", "polling"],
