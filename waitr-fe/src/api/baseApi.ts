abc526d6aef49f702e18d3e03ae561995f6804ae
Aleksandruu <mischie.andrey@gmail.com>
2025-07-28 12:27:51 +0300

    hardcoded api url

--- waitr-fe/src/api/baseApi.ts
+++ waitr-fe/src/api/baseApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
-const apiUrl = import.meta.env.VITE_APP_API_URL;
+const apiUrl = "http://localhost:8080/";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: apiUrl,
